14bd6faa3c5d516949abcedf6e293c3e720a9bd7
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2023-03-26 15:07:15 +0530

    Update extra.js

--- commands/extra.js
+++ commands/extra.js
@@ -2,49 +2,26 @@
 const { fetchJson, cmd, citel, Config } = require('../lib')
 
 cmd({
-
             pattern: "ss",
-
-            desc: "web ss",
-
-            
-
+            desc: "web ss", 
             react: "📸",
-
             category: "downloader"
-
             
-
             
-
         },
-
         async(Void, citel, text) => {
-
             if (!text) return
-
 const webss = await fetchJson(`https://api.botcahx.biz.id/api/tools/ssweb?link=${text}&apikey=${Config.botapikey}`)
-
             citel.reply (`*Screenshot is taking, please wait...*`)
-
        Void.sendMessage(citel.chat, {
-
                 image: {
-
                     url: `https://api.botcahx.biz.id/api/tools/ssweb?link=${text}&apikey=${Config.botapikey}` ,
-
                 },
-
                 caption: `● ᴘᴏᴡᴇʀᴅ ʙʏ ᴘʀᴀʙᴀᴛʜ-ᴍᴅ`,
-
             }, {
-
                 quoted: citel,
-
             });
-
  }
-
 )
 
 
@@ -52,45 +29,50 @@ const webss = await fetchJson(`https://api.botcahx.biz.id/api/tools/ssweb?link=$
 cmd({
 
             pattern: "fb",
-
             desc: "fb down",
-
-            
-
             react: "📥",
-
             category: "downloader"
-
         },
-
         async(Void, citel, text) => {
-
             if (!text) return
-
 const fbdl = await fetchJson(`https://api.botcahx.biz.id/api/dowloader/fbdown?url=${text}&apikey=${Config.botapikey}`)
-
 const videolink = fbdl.result.HD
-
             citel.reply (`*Finding Your Facebook Video* 🌐📡`);
-
        Void.sendMessage(citel.chat, {
-
                 video: {
-
                     url: videolink ,
-
                 },
-
                 caption: `● ᴘᴏᴡᴇʀᴅ ʙʏ ᴘʀᴀʙᴀᴛʜ-ᴍᴅ`,
-
                 footer: `tlang().footer`,
-
             }, {
-
                 quoted: citel,
-
             });
-
  }
+)
 
+
+
+cmd({
+            pattern: "tt",
+            alias: ["tiktok"],
+            desc: "tiktok downloader",
+            react:"🚀",
+            category: "downloader"
+        },
+        async(Void, citel, text) => {
+            if (!text) return
+const ttdl = await fetchJson(`https://api.botcahx.biz.id/api/dowloader/tikok?url=${text}&apikey=818f5a7d`)
+    
+const videolink = ttdl.result.video2
+            citel.reply (`*Finding Your Tiktok Video* 🌐📡`);
+       Void.sendMessage(citel.chat, {
+                video: {
+                    url: videolink ,
+                },
+                
+                caption: `● ᴘᴏᴡᴇʀᴅ ʙʏ ᴘʀᴀʙᴀᴛʜ-ᴍᴅ`,
+            }, {
+                quoted: citel,
+            });
+ }
 )
